390581b3536f8971c80f993a8c269743eb2bf59e
zederer <54798591+delez911@users.noreply.github.com>
2024-05-07 09:30:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
-import streamlit as st 导入streamlitas st
-import pandas as pd 导入 pandasas pd
+import streamlit as st # 导入streamlitas st
+import pandas as pd # 导入 pandasas pd
  
 st.write("""
-# My first app # 我的第一个应用程序
-Hello *world!* 你好世界！*
-""") “””）
+# My first app
+Hello *world!*
+""")
  
 df = pd.read_csv("my_data.csv")
 st.line_chart(df)
